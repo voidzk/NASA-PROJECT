16b74540cb2428730b9fe98c9cb3c880c9c2f152
voidzk <91224200+voidzk@users.noreply.github.com1>
2022-01-25 21:18:53 +0200

    build pipeline workflow.7//added v1 route.2

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -76,7 +76,7 @@ describe('Launches API', () => {
         });
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-type', /json/)
                 .expect(400);
